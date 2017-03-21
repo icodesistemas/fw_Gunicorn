7e9b11b67c6b8715a0e8fe50c55be8135c710f08
abejarano <icode.sistemas@gmail.com>
2017-03-21 16:25:54 -0400

    agregada una funcion para generar nombres random a los constraint

--- kernel/engine/dataBase/Constrainst.php
+++ kernel/engine/dataBase/Constrainst.php
@@ -36,3 +36,14 @@ class Constrainst
             return 'CASCADE';
     }
 }
+
+function getNamerandom(){
+    $caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"; //posibles caracteres a usar
+    $numerodeletras=4; //numero de letras para generar el texto
+    $cadena = ""; //variable para almacenar la cadena generada
+    for($i=0;$i<$numerodeletras;$i++){
+        $cadena .= substr($caracteres,rand(0,strlen($caracteres)),1); /*Extraemos 1 caracter de los caracteres
+			entre el rango 0 a Numero de letras que tiene la cadena */
+    }
+    return $cadena;
+}
