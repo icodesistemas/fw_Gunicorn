a5ec0c08f065666221ec8821a095865213e9edf1
abejarano <icode.sistemas@gmail.com>
2017-03-22 10:12:44 -0400

    implementacion de metodos para comprobar la existencia de campos en un modelo

--- kernel/classes/abstracts/aModels.php
+++ kernel/classes/abstracts/aModels.php
@@ -7,7 +7,7 @@ use fw_Gunicorn\kernel\engine\dataBase\DataBase;
 
 abstract class aModels extends DataBase {
     private $structModel;
-    private $fields = array();
+    private static $fields = array();
     private $model;
     private $uniq;
     private $pk;
@@ -33,11 +33,39 @@ abstract class aModels extends DataBase {
     }
 
     /**
-     * Extrae los campos de la estructura del model, y los coloca en el array fields
+     * Extrae los campos de la estructura del model, y los coloca en el array fields siendo el nombre del campo
+     * la llave y el valor el tipo de dato
      */
     private function extractFields(){
         foreach ($this->structModel as $key => $value){
-            $this->fields[] = trim($key);
+            /* obtiene el tipo de dato, simplicandolos a solo numericos y de cadena */
+
+            if(preg_match('/BIGINT/', $value) ||
+                preg_match('/INTEGER/', $value) ||
+                preg_match('/NUMERIC/', $value) ||
+                preg_match('/REAL/', $value) ||
+                preg_match('/REAL/', $value) ||
+                preg_match('/serial/', $value) ||
+                preg_match('/AUTO INCREMENT/', $value) ||
+                preg_match('/bool/', $value) ||
+                preg_match('/boolean/', $value) ||
+                preg_match('/DECIMAL/', $value)
+            ){
+
+                $tipo_dato = 'NUMERIC';
+            }
+            if(preg_match('/char/', $value) ||
+                preg_match('/text/', $value) ||
+                preg_match('/datetime/', $value) ||
+                preg_match('/timestamp without time zone/', $value) ||
+                preg_match('/date/', $value) ||
+                preg_match('/character varying/', $value) ||
+                preg_match('/varchar/', $value)
+            ){
+                $tipo_dato = 'STRING';
+            }
+            aModels::$fields[] = [trim($key) => $tipo_dato];
+            #$this->fields[] = trim($key);
         }
     }
     private function setTable($table){
@@ -50,7 +78,7 @@ abstract class aModels extends DataBase {
      * @return $this
      */
     public function where(Array $conditions){
-        $this->where = $this->renderWhere($conditions);
+        $this->_where = $this->renderWhere($conditions);
 
         return $this;
     }
@@ -58,22 +86,63 @@ abstract class aModels extends DataBase {
     /**
      * Verifica que los campos involucrados en la condicion existen en el modelo
      * @param $conditions array de condiciones que se colocan en la clausula where del query
+     * @return Retorna el where listo para ser usado por el query
      */
     private function renderWhere($conditions){
-
+        $where = " where ";
         foreach ($conditions as $key => $value){
 
-            $condition = explode('=', $value);
-            $field_condition = $condition[0];
+            if(preg_match('/=/', $value)){
+                $condition = explode('=', $value);
+                $field_condition = trim($condition[0]);
+            }
 
-            if(!in_array($field_condition, $this->fields))
-                die("Sorry, the field <strong>$field_condition</strong> not exists in the model <strong>$this->model</strong> ");
 
+            /* si tiene un or, in, not in elimina el signo de llaves */
+            if(preg_match('/{or}/', $value))
+                $value =  str_replace('{or}', 'OR', $value);
+            else if(preg_match('/{in}/', $value))
+                $value =  str_replace('{in}', 'IN', $value);
+            else if(preg_match('/{not in}/', $value))
+                $value =  str_replace('{in}', 'NOT IN', $value);
+            else
+                aModels::findFieldModel($field_condition);
+
+            $where .= " $value and ";
         }
+        $where = trim($where,' and ');
+
+        if ($where == " where ")
+            return "";
+        else
+            return $where;
+
     }
 
+    /**
+     * busca si un campo pasado por pasametro es realmente campo del model
+     * @param $name_field nombre del campo que se desea buscar
+     * @return bool True si el campo pertenece al modelo y False y no pertenece
+     */
+    static function findFieldModel($name_field){
+        foreach (aModels::$fields as $value){
+            foreach ($value as $field => $type){
+                if($name_field == $field){
+                    return $type;
+                }
+            }
+        }
+        throw new \Exception("The field $name_field does not exist in the model ");
+    }
     protected function __getFieldsModel(){
-        return $this->fields;
+        $fields = "";
+        foreach (aModels::$fields as $value){
+            foreach ($value as $field => $type){
+                $fields .= $field.  ', ';
+            }
+        }
+
+        return trim($fields, ', ');
     }
     protected function __getNameModel(){
         return $this->model;
